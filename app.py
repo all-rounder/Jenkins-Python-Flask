47ca21628970d2f1d1d1aa52ef5b26e2925c1d73
all-rounder <xiaoyao160@gmail.com>
2023-07-31 23:37:30 +1000

    add files

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
-#!/usr/bin/env python
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index_page():
-    return 'This is a test page for CI/CD.\n'
-
-
-@app.route('/hello/')
-def hello_world():
-    return 'Hello World!\n'
-
-
-@app.route('/hello/<username>')  # dynamic route
-def hello_user(username):
-    return 'Why Hello %s!\n' % username
+def home():
+    return "<h1>Welcome to Jenkins Tutorials<h1/>"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')  # open for everyone
+    app.run(debug=True, host='0.0.0.0')
